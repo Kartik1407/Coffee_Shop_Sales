9c6db6f782a19b30fc74365e239dd8d3930ed4e1
Kartik1407 <kartik.gupta.1407@gmail.com>
2025-01-28 18:05:07 +0530

    Update CH1.sql

--- Solution/CH1.sql
+++ Solution/CH1.sql
@@ -1,9 +1,1 @@
--- Chart requirement: Implement tooltips to display detailed metrics when hovering over a specific day
 
-select
-concat(round(sum(unit_price * transaction_qty)/1000,2),'K') as Total_sales,
-concat(round(sum(transaction_qty)/1000,2),'K') as Total_qty_sold,
-concat(round(count(transaction_id)/1000,2),'K') as Total_orders
-from coffee_shop
-where									-- filters
- transaction_date = '2023-05-18'; 
